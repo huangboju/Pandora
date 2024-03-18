c69822705b42fc1b21edb33347a31433fb079837
xiAo-Ju <xiao-ju@foxmail.com>
2024-03-18 11:51:11 +0800

    feat:  删除没用代码

--- Demos/JSCore/Swift-JavascriptDemo-master/JSCore/AppDelegate.swift
+++ Demos/JSCore/Swift-JavascriptDemo-master/JSCore/AppDelegate.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum GoodsDetailDeeplinkHost: String, CaseIterable {
+    case goodsDetail = "goods_detail"
+    case miniGoodsDetail = "mini_goods_detail"
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,8 +20,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let link = "xhsdiscover://goods_detail/639c68f7af570300017e4c66?max_width=800&listing_image=https%3A%2F%2Fqimg.xiaohongshu.com%2Fmaterial_space%2F8b8a78e4-ea50-42cb-b902-c27d28f1854a%3FitemId%3D639c68f7af570300017e4c66%26imageView2%2F1%2Fw%2F620%2Fh%2F620%2Fq%2F90.jpeg&custom_backgroundColor=%23ffffff&prefetch-image=https%3A%2F%2Fqimg.xiaohongshu.com%2Fmaterial_space%2F8b8a78e4-ea50-42cb-b902-c27d28f1854a%3FitemId%3D639c68f7af570300017e4c66%26imageView2%2F2%2Fw%2F800%2Fq%2F90.jpeg&trackId=lns_lns@64abcc606c3563000d8c254e&max_height=800&source=mall_home&first_width=800&first_height=800&has_video=false"
+        reportGoodsCardClickedIfNeeded(link)
         return true
+    }
+
+    func reportGoodsCardClickedIfNeeded(_ deeplink: String) {
+        guard var com = URLComponents(string: deeplink) else { return }
+        let hostSet = Set(GoodsDetailDeeplinkHost.allCases.map {
+            $0.rawValue
+        })
+        print(com.scheme == "xhsdiscover")
+        if let host = com.host, !hostSet.contains(host) {
+            return
+        }
+        let ignoreKeySet = Self.ignoreKeySet
+        let result = com.queryItems?.filter { !ignoreKeySet.contains($0.name) }
+        com.queryItems = result
+        let path = com.path.replacingOccurrences(of: "/", with: "")
+        let query = com.query.map { $0 + "&goodsId=\(path)" } ?? ""
+        print(query)
+    }
+
+    static var ignoreKeySet: Set<String> {
+        [
+            "key_raw_url",
+            "statusBar",
+            "needNewRootNavigation",
+            "halfShadowCorner",
+            "halfShadowHeight",
+            "listing_image",
+            "halfShadowColor",
+            "first_width",
+            "halfShadow",
+            "prefetch-image",
+            "first_height",
+            "max_width",
+            "background_transparent_v2",
+            "max_height",
+            "rnAnimated",
+            "custom_backgroundColor"
+        ]
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
